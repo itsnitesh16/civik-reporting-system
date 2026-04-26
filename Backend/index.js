6c26559843da0c8d7d67540017743daa22becf1b
itsnitesh16 <niteshkumarghn1@gmail.com>
2026-04-26 15:25:59 +0530

    start script added

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const  dotenv = require("dotenv");
-const mongoose=require("mongoose");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const User = require('./models/user');
-const cors=  require("cors");
+const cors = require("cors");
 dotenv.config();
 const app = express();
-const Routes= require('./routes/user');
+const Routes = require('./routes/user');
 app.use(cors({
   origin: ['http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
 
-app.use('/',Routes);
+app.use('/', Routes);
 
 
 
